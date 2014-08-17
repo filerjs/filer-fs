a62c2b42ba9aeb066991747d2c057565a63d7d76
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2014-08-16 21:06:05 -0400

    Rewrite for getObject/getBuffer and putObject/putBuffer API

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var os = require('os');
 var fs = require('fs');
 var path = require('path');
-var async = require('async');
 var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
 
 function FSContext(options) {
   this.readOnly = options.isReadOnly;
@@ -13,39 +13,37 @@ function prefixKey(prefix, key) {
   return path.join(prefix, key);
 }
 
-FSContext.prototype.put = function (key, value, callback) {
+function _put(keyPrefix, key, value, callback) {
   if(this.readOnly) {
     return callback("Error: Write operation on readOnly context.");
   }
-  key = prefixKey(this.keyPrefix, key);
-  // We do extra work to make sure typed buffer survive
-  // being stored on disk and still get the right prototype later.
-  if (Buffer.isBuffer(value)) {
-    value = {
-      __isBuffer: true,
-      __array: value.toJSON()
-    };
-  }
-  value = JSON.stringify(value);
 
-  fs.writeFile(key, value, function(err) {
+  var keyPath = prefixKey(keyPrefix, key);
+  fs.writeFile(keyPath, value, function(err) {
     if(err) {
-      callback("Error: unable to write to disk. Error was " + err);
-      return;
+      return callback("Error: unable to write to disk. Error was: " + err.stack);
     }
+
     callback();
   });
+}
+FSContext.prototype.putObject = function(key, value, callback) {
+  var json = JSON.stringify(value);
+  _put(this.keyPrefix, key, json, callback);
+};
+FSContext.prototype.putBuffer = function(key, value, callback) {
+  _put(this.keyPrefix, key, value, callback);
 };
 
 FSContext.prototype.delete = function (key, callback) {
   if(this.readOnly) {
     return callback("Error: Write operation on readOnly context.");
   }
-  key = prefixKey(this.keyPrefix, key);
-  fs.unlink(key, function(err) {
-    if(err) {
-      callback("Error: unable to delete key from disk. Error was " + err);
-      return;
+
+  var keyPath = prefixKey(this.keyPrefix, key);
+  fs.unlink(keyPath, function(err) {
+    if(err && err.code !== 'ENOENT') {
+      return callback("Error: unable to delete key from disk. Error was " + err);
     }
     callback();
   });
@@ -56,59 +54,39 @@ FSContext.prototype.clear = function (callback) {
     return callback("Error: Write operation on readOnly context.");
   }
 
-  function removeEntries(pathname, callback) {
-    fs.readdir(pathname, function(error, entries) {
-      if(error) {
-        callback(error);
-        return;
-      }
-
-      entries = entries.map(function(filename) {
-        return path.join(pathname, filename);
-      });
-
-      async.each(entries, function(filename, callback) {
-        fs.unlink(filename, function(err) {
-          if(err) {
-            callback(err);
-            return;
-          }
-        });
-      },
-      function(error) {
-        if(error) {
-          callback(error);
-          return;
-        }
-      });
-      callback(null);
-    });
-  }
-
-  removeEntries(this.keyPrefix, callback);
+  // rm -fr <user/fs/dir/root>
+  rimraf(this.keyPrefix, callback);
 };
 
-FSContext.prototype.get = function (key, callback) {
-  key = prefixKey(this.keyPrefix, key);
-  fs.readFile(key, 'utf8', function(err, data) {
+function _get(keyPrefix, encoding, key, callback) {
+  var keyPath = prefixKey(keyPrefix, key);
+  fs.readFile(keyPath, {encoding: encoding}, function(err, data) {
     if(err && err.code !== 'ENOENT') {
-      callback("Error: unable to get key from disk. Error was " + err);
-      return;
+      return callback("Error: unable to get key from disk. Error was " + err);
     }
 
-    try {
-      if(data) {
-        data = JSON.parse(data);
-        // Deal with special-cased flattened typed buffer (see put() below)
-        if(data.__isBuffer) {
-          data = new Buffer(data.__array);
-        }
-      }
-      callback(null, data);
-    } catch(e) {
-      return callback(e);
-    }
+    callback(null, data);
   });
+}
+FSContext.prototype.getObject = function(key, callback) {
+  _get(this.keyPrefix, 'utf8', key, function(err, data) {
+    if(err) {
+      return callback(err);
+    }
+
+    if(data) {
+      try {
+        data = JSON.parse(data);
+      } catch(e) {
+        return callback(e);
+      }
+    }
+
+    callback(null, data);
+  });
+};
+FSContext.prototype.getBuffer = function(key, callback) {
+  _get(this.keyPrefix, /* leave as raw Buffer */ null, key, callback);
 };
 
 function FSProvider(options) {
@@ -123,24 +101,26 @@ FSProvider.isSupported = function() {
 
 FSProvider.prototype.open = function(callback) {
   if(!this.keyPrefix) {
-    callback("Error: Missing keyPrefix");
-    return;
+    return callback("Error: Missing keyPrefix");
   }
+
   var that = this;
   var dir = path.join(this.root, this.keyPrefix);
 
   mkdirp(dir, function(err) {
     if (err && err.code != 'EEXIST') {
-      callback(err);
-      return;
+      return callback(err);
     }
+
     that.keyPrefix = dir;
 
+    // Check for any nodes. We need a least a supernode
+    // or this will need to be formatted.
     fs.readdir(dir, function(err, entries) {
       if(err) {
-        callback(err);
-        return;
+        return callback(err);
       }
+
       callback(null, entries.length === 0);
     });
   });
